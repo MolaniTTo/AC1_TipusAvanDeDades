6f8551653877cc635f9a7d07cd1fd8f618d493b6
arnauitb <arnau.molano.7e7@itb.cat>
2024-04-02 12:59:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml.Schema;
+using AC1_TipusAvançatsDeDades;
+
+namespace AC1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<Score> scores = new List<Score>();
+
+            for (int i = 0; i <10; i++)
+            {
+                Console.WriteLine("Player:");
+                string player = Console.ReadLine();
+                Console.WriteLine("Mission:");
+                string mission = Console.ReadLine();
+                Console.WriteLine("Scoring:");
+                int scoring = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Player {i + 1} added!");
+
+                Score score = new Score
+                {
+                    Player = player,
+                    Mission = mission,
+                    Scoring = scoring
+                };
+
+                scores.Add(score);
+            }
+
+            
+
+
+        }
+    }
+}
---
+++ Score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC1_TipusAvançatsDeDades
+{
+    public class Score
+    {
+        public string Player {  get; set; }
+        public string Mission { get; set; }
+        public int Scoring { get; set; }
+
+
+    } 
+
+
+
+}
